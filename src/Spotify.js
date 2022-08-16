16882104732949fddb5a0179609608e27dfb4745
jeannotds <diambujeannot@gmail.com>
2022-08-16 20:48:33 +0100

    Ajout du fonction getTokenFromUrl dans Spotify.js

--- src/Spotify.js
+++ src/Spotify.js
@@ -14,4 +14,27 @@ const scopes = [
     "user-modify-playback-state",
 ];
 
+export const getTokenFromUrl = () =>{
+    return window.location.hash
+
+    /*
+        https://www.youtube.com/watch?v=176JPkG227g
+        https://bobbyhadz.com/blog/react-substring-method
+    */
+    .substring(1)
+    .split('&')
+    .reduce((initial, item) =>{
+    
+    /*
+    // https://fr.acervolima.com/difference-entre-les-fonctions-decodeuricomponent-et-decodeuri-en-javascript/#:~:text=decodeURI()
+    %20%3A%20il%20prend%20la,et%20renvoie%20la%20string%20d%C3%A9cod%C3%A9e.
+    */
+        let parts = item.split('=')
+        initial[parts[0]] = decodeURIComponent(parts[1])
+
+        return initial;
+
+    }, {});
+}
+ 
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`
