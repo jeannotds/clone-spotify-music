24448bd86922aa199e71903b1988fc5bfef2830a
jeannotds <diambujeannot@gmail.com>
2022-08-16 11:30:19 +0100

    Initialisation du projet

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       We are builder clone spotify
+      {/* spotify logo */}
+      {/* login with spotify */}
+
+      <Login /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Login.js
@@ -0,0 +1,14 @@
+
+
+
+
+// function Login(){
+
+
+//     return(
+//         <div>
+//             {/*  Spotify logo  */}
+//             {/* Login with spotify botton */}
+//         </div>
+//     )
+// }
