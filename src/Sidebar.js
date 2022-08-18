e53af58091aa1ed711f091bf93411b2bed3ef383
jeannotds <diambujeannot@gmail.com>
2022-08-18 12:14:06 +0100

    Ajout de menu PlayLists

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,6 +4,7 @@ import SidebarOption from './SidebarOption'
 import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
 import SearchIcon from '@mui/icons-material/Search';
 import HouseIcon from '@mui/icons-material/House';
+import { PlaylistAdd } from '@mui/icons-material';
 
 function Sidebar() {
   return (
@@ -11,18 +12,21 @@ function Sidebar() {
         <img  className='sidebar__logo'
             src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" 
         />
-        <SidebarOption Icon={LibraryBooksIcon} title="Home" />
+        
+        <SidebarOption Icon={HouseIcon} title=" Home" />
         <SidebarOption Icon={SearchIcon} title="Search" />
-        <SidebarOption Icon={HouseIcon} title="Your library" />
+        <SidebarOption Icon={LibraryBooksIcon} title="Your library" />
 
         <br/>
-
         <strong className='sidebar__title'>PLAYLISTS</strong>
-
         <hr/>
+
+        <SidebarOption title='Hip hop' />
+        <SidebarOption title='Rock' />
+        <SidebarOption title='RnB' />
 
     </div>
   )
 }
- 
+
 export default Sidebar
